dff22db10a24431e0adfa521ef40f35a834c2d70
Cedric GOUBARD <c.goubard21@imperial.ac.uk>
2024-02-07 15:48:03 +0000

    Driver working

--- src/pupil_ros/driver.py
+++ src/pupil_ros/driver.py
@@ -9,11 +9,15 @@ from pupil_labs.realtime_api.simple import discover_one_device
 class PupilAsyncDriver(object):
     def __init__(self):
         self._discover_device()
-        self._generate_event_srvs()#
+        self._generate_event_srvs()
 
         self.bridge = CvBridge()
         self.scene_pub = rospy.Publisher("~scene_image", Image, queue_size=5)
         self.rate = rospy.get_param("~rate", 30)
+        self.stream_img = rospy.get_param("~stream_img", False)
+
+        self.start_rec_srv = rospy.Service("~start_recording", Trigger, self._start_recording)
+        self.stop_rec_srv = rospy.Service("~stop_recording", Trigger, self._stop_recording)
 
     def __del__(self):
         self.device.close()
@@ -31,7 +35,11 @@ class PupilAsyncDriver(object):
             {"srv_name": "start_session", "event_name": "session.begin"},
             {"srv_name": "stop_session", "event_name": "session.end"},
             {"srv_name": "start_intervention", "event_name": "intervention.begin"},
-            {"srv_name": "stop_intervention", "event_name": "intervention.end"}
+            {"srv_name": "stop_intervention", "event_name": "intervention.end"},
+            {"srv_name": "start_movement", "event_name": "move.begin"},
+            {"srv_name": "end_movement", "event_name": "move.end"},
+            {"srv_name": "report_failure", "event_name": "failure"},
+            {"srv_name": "report_success", "event_name": "success"},
         ]
 
         for srv_dict in self.events_srvs:
@@ -41,12 +49,12 @@ class PupilAsyncDriver(object):
         self.device.send_event(msg)
         return TriggerResponse(success=True)
     
-    def _start_recording(self, req, msg):
+    def _start_recording(self, req):
         rid = self.device.recording_start()
         rospy.loginfo("Started recording with id: %s", rid)
         return TriggerResponse(success=True)
 
-    def _stop_recording(self, req, msg):
+    def _stop_recording(self, req):
         self.device.recording_stop_and_save()
         rospy.loginfo("Stopped recording.")
         return TriggerResponse(success=True)
@@ -54,13 +62,12 @@ class PupilAsyncDriver(object):
     def spin(self):
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
-            tstp = rospy.Time.now()
-            frame, gaze = self.device.receive_matched_scene_video_frame_and_gaze()
-            if frame is not None:
-                scene_msg = self.bridge.cv2_to_imgmsg(frame.bgr_pixels, encoding="bgr8")
-                scene_msg.header.stamp = tstp
-                self.scene_pub.publish(scene_msg)
-
-            # if gaze is not None:
+            if self.stream_img:
+                tstp = rospy.Time.now()
+                frame, gaze = self.device.receive_matched_scene_video_frame_and_gaze()
+                if frame is not None:
+                    scene_msg = self.bridge.cv2_to_imgmsg(frame.bgr_pixels, encoding="bgr8")
+                    scene_msg.header.stamp = tstp
+                    self.scene_pub.publish(scene_msg)
 
             rate.sleep()
